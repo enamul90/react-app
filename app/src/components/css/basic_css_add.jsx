9acf6f9b409ef1534dd2c609a3df55fe3f307951
md enamul <hfiroz1998@gmail.com>
2024-10-14 09:23:47 +0600

    add css

--- app/src/components/css/basic_css_add.jsx
+++ app/src/components/css/basic_css_add.jsx
@@ -1,12 +1,13 @@
 
 import './index.css'
 
+
 function basic_css_add() {
 
   return (
     <>
-    <h1 ClassName='tittle' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, culpa.</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima error magnam totam suscipit rerum! Nisi, nemo cupiditate similique alias ducimus quam quis reiciendis! Necessitatibus alias quibusdam quos atque aut.</p>
+    <h1 className='tittle' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, culpa.</h1>
+      <p className='color'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima error magnam totam suscipit rerum! Nisi, nemo cupiditate similique alias ducimus quam quis reiciendis! Necessitatibus alias quibusdam quos atque aut.</p>
       <button>my button</button>
     </>
   )
