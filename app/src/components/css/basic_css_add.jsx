dbc27445a04c415dd213cb59f3bc4a121647a1f8
md enamul <hfiroz1998@gmail.com>
2024-10-14 08:59:55 +0600

    add to my computer

--- app/src/components/css/basic_css_add.jsx
+++ app/src/components/css/basic_css_add.jsx
@@ -1,11 +1,11 @@
 
-// import 'index.css'
+import './index.css'
 
 function basic_css_add() {
 
   return (
     <>
-    <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, culpa.</h1>
+    <h1 ClassName='tittle' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, culpa.</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci minima error magnam totam suscipit rerum! Nisi, nemo cupiditate similique alias ducimus quam quis reiciendis! Necessitatibus alias quibusdam quos atque aut.</p>
       <button>my button</button>
     </>
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Basic_Css from './assets/Components/css/basic_css_add'
+import AddCSS from './components/css/basic_css_add.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Basic_Css />
+  <AddCSS />
+
+
   </StrictMode>,
 )
-
