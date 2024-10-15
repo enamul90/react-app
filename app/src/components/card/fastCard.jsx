740c3711ae514556a8c4d08583e8034bd66691e5
md enamul <hfiroz1998@gmail.com>
2024-10-15 23:50:24 +0600

    create 2 card - use css

--- app/src/components/card/fastCard.jsx
+++ app/src/components/card/fastCard.jsx
@@ -1,8 +1,7 @@
+import style from "./card.module.css"
 
-import style from './basic.module.css';
 
-
-function basic_css_add() {
+function fastCard() {
 
   const img = 'https://scontent.fdac138-1.fna.fbcdn.net/v/t45.5328-4/462471512_1654965965401474_3846402229157774181_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=247b10&_nc_eui2=AeHp9cJcnbPoGjJnbk7Lf3zuFqbbCsF-qZwWptsKwX6pnIeFzGlpM-UNnRySbkMOvChqBj9NspEdGKyqL93u7H1P&_nc_ohc=M6RllsJSzn8Q7kNvgHhiFRH&_nc_zt=23&_nc_ht=scontent.fdac138-1.fna&_nc_gid=ADBv_Aq80rQ7iOe0-_4bAjs&oh=00_AYAUSo_5Gcp_qsVnews2jMsiWf4VjzY29h8duU24rrBy9Q&oe=67146BAE'
   const tittle = 'This is our product tittle and product variations'
@@ -11,7 +10,9 @@ function basic_css_add() {
   return (
     <>
       <div className={style.cardBody}>
-        <img src={img} alt="Product" />
+        <div className={style.imgBox}>
+          <img src={img} alt="Product" />
+        </div>
         <div className={style.cardText}>
           <h1>{tittle}</h1>
           <p >{paragraph}</p>
@@ -23,5 +24,4 @@ function basic_css_add() {
   )
 }
 
-export default basic_css_add
-
+export default fastCard
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import AddCSS from './components/css/basic_css_add.jsx'
+// import AddCSS from './components/css/basic_css_add.jsx'
+import FastCard from './components/card/fastCard.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <AddCSS />
-
+  {/* <AddCSS /> */}
+  <FastCard />
 
   </StrictMode>,
 )
